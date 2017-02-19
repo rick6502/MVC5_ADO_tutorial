2e3e49455c082d9655648d1a62f8c7787372183b
rick6502 <rick2032@hotmail.com>
2017-02-19 13:22:37 -0800

    Finishing step 4 of adding the EmpController and replacing default method with AddNewEmployee metho

--- MVC5_ADO_tutorial/Controllers/EmpController.cs
+++ MVC5_ADO_tutorial/Controllers/EmpController.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using MVC5_ADO_tutorial.Models;
+
 namespace MVC5_ADO_tutorial.Controllers
 {
     public class EmpController : Controller
     {
-        // GET: Emp
-        public ActionResult Index()
+        public ActionResult AddNewEmployee(EmpModel Emp)
         {
+
             return View();
+
         }
     }
 }
