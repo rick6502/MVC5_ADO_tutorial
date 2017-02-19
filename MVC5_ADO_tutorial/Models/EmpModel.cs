5caab73e82433aec42095f27fd7f342e22e16503
rick6502 <rick2032@hotmail.com>
2017-02-19 12:59:03 -0800

    Add EmpModel details

--- MVC5_ADO_tutorial/Models/EmpModel.cs
+++ MVC5_ADO_tutorial/Models/EmpModel.cs
@@ -3,9 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace MVC5_ADO_tutorial.Models
 {
     public class EmpModel
     {
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FName
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Middle Name")]
+        public string MName
+        {
+            get; set;
+
+        }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LName
+        {
+            get; set;
+        }
+
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid emaild address")]
+        [Required(ErrorMessage = "Emailld is required.")]
+        public string EmailId
+        {
+            get; set;
+        }
     }
 }
