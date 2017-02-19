a43133acca2e9b69af14f169b376ef5d79fc0bb7
rick6502 <rick2032@hotmail.com>
2017-02-19 13:19:09 -0800

    Add EmpController

---
+++ MVC5_ADO_tutorial/Controllers/EmpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_ADO_tutorial.Controllers
+{
+    public class EmpController : Controller
+    {
+        // GET: Emp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
