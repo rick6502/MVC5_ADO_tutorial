7626a104a4b2d3d3be8444f1c9586b37e5270839
rick6502 <rick2032@hotmail.com>
2017-02-21 21:11:03 -0800

    Error found in Employee table defintion; forgot to set identity attribute on the ID field.  Fixed.

--- MVC5_ADO_tutorial/dbo.EmployeeTable.sql
+++ MVC5_ADO_tutorial/dbo.EmployeeTable.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Employee]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT  IDENTITY (1, 1) NOT NULL PRIMARY KEY, 
     [FName] VARCHAR(50) NULL, 
     [MName] VARCHAR(50) NULL, 
     [LName] VARCHAR(50) NULL, 
