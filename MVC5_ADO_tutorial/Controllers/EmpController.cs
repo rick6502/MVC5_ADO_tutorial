f871406551426f035e3862354c3bb6cd83ac6512
rick6502 <rick2032@hotmail.com>
2017-02-21 21:03:20 -0800

    Forgot inclucde the ConnectionString in the main Webconfig which resulted in error.  Fixed.

--- MVC5_ADO_tutorial/Controllers/EmpController.cs
+++ MVC5_ADO_tutorial/Controllers/EmpController.cs
@@ -6,15 +6,56 @@ using System.Web.Mvc;
 
 using MVC5_ADO_tutorial.Models;
 
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
 namespace MVC5_ADO_tutorial.Controllers
 {
     public class EmpController : Controller
     {
+
+
+        private SqlConnection con;
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+
+
+        ///<summary>  
+        ///Action method which  
+        ///insert the data into database by capturing  
+        ///Model values which comes from user as input  
+        ///</summary>  
         public ActionResult AddNewEmployee(EmpModel Emp)
         {
+            //To Prevent firing validation error on first page Load  
 
+            if (ModelState.IsValid)
+            {
+                connection();
+                SqlCommand com = new SqlCommand("InsertData", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@FName", Emp.FName);
+                com.Parameters.AddWithValue("@MName", Emp.MName);
+                com.Parameters.AddWithValue("@LName", Emp.LName);
+                com.Parameters.AddWithValue("@EmailId", Emp.EmailId);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+                if (i >= 1)
+                {
+                    ViewBag.Message = "New Employee Added Successfully";
+                                    }
+
+
+            }
+            ModelState.Clear();
             return View();
-
+            
         }
     }
 }
